d30a891e13fb584c4f39a3f55dc3c19af1cf804e
Enrique Diaz <nrikediaz@gmail.com>
2011-02-17 03:07:03 -0600

    Living a hell :S

--- gen/com/blablahlabs/excelsior/R.java
+++ gen/com/blablahlabs/excelsior/R.java
@@ -10,42 +10,74 @@ package com.blablahlabs.excelsior;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background1=0x7f040005;
+        public static final int background2=0x7f040006;
+        public static final int background3=0x7f040007;
+        public static final int foreground1=0x7f040003;
+        public static final int foreground2=0x7f040004;
+        public static final int pinned_header_background=0x7f04000a;
+        public static final int session_foreground_past=0x7f040009;
+        public static final int title_background=0x7f040000;
+        public static final int title_separator=0x7f040008;
+        public static final int title_text=0x7f040001;
+        public static final int title_text_alt=0x7f040002;
+    }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int logo_excelsior_small=0x7f020001;
+        public static final int b1_s1=0x7f020000;
+        public static final int b1a_s1=0x7f020001;
+        public static final int b2_s1=0x7f020002;
+        public static final int b2a_s1=0x7f020003;
+        public static final int b3_s1=0x7f020004;
+        public static final int b3a_s1=0x7f020005;
+        public static final int b4_s1=0x7f020006;
+        public static final int b4a_s1=0x7f020007;
+        public static final int b5_s1=0x7f020008;
+        public static final int b5a_s1=0x7f020009;
+        public static final int b6_s1=0x7f02000a;
+        public static final int b6a_s1=0x7f02000b;
+        public static final int b7_s1=0x7f02000c;
+        public static final int b7a_s1=0x7f02000d;
+        public static final int b8_s1=0x7f02000e;
+        public static final int b8a_s1=0x7f02000f;
+        public static final int header=0x7f020010;
+        public static final int icon=0x7f020011;
+        public static final int logo_excelsior_small=0x7f020012;
+        public static final int menu_info=0x7f020013;
+        public static final int menu_news=0x7f020014;
     }
     public static final class id {
-        public static final int ButtonsLinearLayout=0x7f070002;
-        public static final int HorizontalScrollView=0x7f070001;
-        public static final int ParentRelativeLayout=0x7f070000;
-        public static final int adrenaline_button=0x7f070008;
-        public static final int community_button=0x7f070007;
-        public static final int excelsior_logo_small=0x7f07000d;
-        public static final int global_button=0x7f070005;
-        public static final int icon=0x7f07000e;
-        public static final int icontext=0x7f070010;
-        public static final int last_news_button=0x7f070003;
-        public static final int money_button=0x7f070006;
-        public static final int multimedia_button=0x7f07000c;
-        public static final int national_button=0x7f070004;
-        public static final int opinion_button=0x7f07000a;
-        public static final int show_button=0x7f070009;
-        public static final int supplements_button=0x7f07000b;
-        public static final int text=0x7f07000f;
+        public static final int ButtonsLinearLayout=0x7f080002;
+        public static final int HorizontalScrollView=0x7f080001;
+        public static final int ParentRelativeLayout=0x7f080000;
+        public static final int adrenaline=0x7f080008;
+        public static final int community=0x7f080007;
+        public static final int excelsior_logo_small=0x7f08000c;
+        public static final int global=0x7f080005;
+        public static final int icon=0x7f08000d;
+        public static final int icontext=0x7f08000f;
+        public static final int last_hour=0x7f080003;
+        public static final int money=0x7f080006;
+        public static final int multimedia_button=0x7f08000b;
+        public static final int national=0x7f080004;
+        public static final int opinion=0x7f08000a;
+        public static final int show=0x7f080009;
+        public static final int text=0x7f08000e;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int window_title=0x7f030001;
     }
     public static final class menu {
-        public static final int menu=0x7f060000;
+        public static final int menu=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
+        public static final int app_name=0x7f060001;
+        public static final int hello=0x7f060000;
     }
     public static final class style {
-        public static final int CustomTheme=0x7f040001;
-        public static final int CustomWindowTitleBackground=0x7f040000;
+        public static final int CustomTheme=0x7f050001;
+        public static final int CustomWindowTitleBackground=0x7f050000;
+        public static final int HomeButton=0x7f050002;
     }
 }
